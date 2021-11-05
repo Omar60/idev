3ac6e9a70771d1ab9796d428a782ae96b1e9f7e1
Omar60 <omar60@live.com.mx>
2021-11-05 13:22:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const serverUrl = "https://kupkethwblgf.usemoralis.com:2053/server";
 const appId = "YpYvIbs7kp82Zl7zdKwVB4RUJl06nfGDEggQ0cwiF";
 Moralis.start({ serverUrl, appId });
 
-/* TODO: Add Moralis Authentication code */
+/* TODO: Codigo de auntentificacion de cuenta y salida*/
 async function login() {
     let user = Moralis.User.current();
     if (!user) {
-      user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+      user = await Moralis.authenticate({ signingMessage: "Acceso a iTest" })
         .then(function (user) {
           console.log("logged in user:", user);
           console.log(user.get("ethAddress"));
