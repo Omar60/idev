742c88a7e278f26871ddee76313cb0cc90dd6a37
Omar60 <omar60@live.com.mx>
2021-11-05 16:43:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Moralis init code */
 const serverUrl = "https://kupkethwblgf.usemoralis.com:2053/server";
-const appId = "YpYvIbs7kp82Zl7zdKwVB4RUJl06nfGDEggQ0cwiF";
+const appId = "pYvIbs7kp82Zl7zdKwVB4RUJl06nfGDEggQ0cwiF";
 Moralis.start({ serverUrl, appId });
 
 /* TODO: Codigo de auntentificacion de cuenta y salida*/
