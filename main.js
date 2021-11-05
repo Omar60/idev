1ecda058a11f8ac8dccccbcd3275594bb4aeffa9
Omar60 <omar60@live.com.mx>
2021-11-05 13:18:21 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+/* Moralis init code */
+const serverUrl = "https://kupkethwblgf.usemoralis.com:2053/server";
+const appId = "YpYvIbs7kp82Zl7zdKwVB4RUJl06nfGDEggQ0cwiF";
+Moralis.start({ serverUrl, appId });
+
+/* TODO: Add Moralis Authentication code */
