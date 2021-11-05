a112d77719c25d6b14773673a3f3e7a5040aae1f
Omar60 <omar60@live.com.mx>
2021-11-05 15:28:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /* Moralis init code */
 const serverUrl = "https://kupkethwblgf.usemoralis.com:2053/server";
 const appId = "YpYvIbs7kp82Zl7zdKwVB4RUJl06nfGDEggQ0cwiF";
-const balance = await Moralis.Web3API.account.getNativeBalance();
 Moralis.start({ serverUrl, appId });
 
 /* TODO: Codigo de auntentificacion de cuenta y salida*/
@@ -25,5 +24,3 @@ async function logOut() {
 }
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
-
-/* Obttener balance nativo */
