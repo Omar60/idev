fb38ac13d8ece602fb6661b1bffe98a8300f9b4e
Omar60 <omar60@live.com.mx>
2021-11-05 15:06:30 -0600

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,6 +1,3 @@
 // get mainnet native balance for the current user
 const balance = await Moralis.Web3API.account.getNativeBalance();
 
-// get BSC native balance for a given address
-const options = { chain: "bsc", address: "0x...", to_block: "1234" };
-const balance = await Moralis.Web3API.account.getNativeBalance(options);
