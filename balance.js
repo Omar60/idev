733f1a6b8534e57052ee0b0a9784c361668ae263
Omar60 <omar60@live.com.mx>
2021-11-05 15:04:32 -0600

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,13 +1,6 @@
-Moralis.initialize("5JNM9X1Nq8ciDngst2pNBUrSoThG5a7jBPYUvrZM"); // Application id from moralis.io
-Moralis.serverURL = "https://xsmb9wqekxnl.moralishost.com:2053/server"; //Server url from moralis.io
+// get mainnet native balance for the current user
+const balance = await Moralis.Web3API.account.getNativeBalance();
 
-async function getBalance() {
-  balance = await Moralis.Web3API.account.getNativeBalance({
-    chain: "bsc",
-    address: "0x67422A4b99D5a22094e54D4e7Ba2Da36431d5C50",
-  });
-  console.log(balance);
-  let ethBalance = balance.balance + " BNB"
-  document.getElementById("balance").innerHTML = ethBalance;
-}
-getBalance();
+// get BSC native balance for a given address
+const options = { chain: "bsc", address: "0x...", to_block: "1234" };
+const balance = await Moralis.Web3API.account.getNativeBalance(options);
